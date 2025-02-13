c78829c91e70e66960a2db4ecd993eb7537f5d85
m1drage <m1drage@mail.ru>
2025-02-13 13:02:04 +0500

    лаба1

---
+++ 00_distance.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Есть словарь координат городов
+
+sites = {
+    'Moscow': (550, 370),
+    'London': (510, 510),
+    'Paris': (480, 480),
+}
+
+# Составим словарь словарей расстояний между ними
+# расстояние на координатной сетке - ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
+
+distances = {}
+
+
+# TODO здесь заполнение словаря
+distances = {
+    city1: {
+        city2: round(((x1 - x2)** 2 + (y1 - y2)**2) ** 0.5, 2)
+        for (city2, (x2, y2)) in sites.items() if city1 != city2
+    }
+    for (city1, (x1, y1)) in sites.items()
+}
+
+print(distances)
+
+
+
+
