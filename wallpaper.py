777ea0d8a8de7603ea5e1e58f510d0a826d71ba7
m1drage <m1drage@mail.ru>
2025-04-02 16:14:42 +0500

    Delete wallpaper.py

--- wallpaper.py
+++
@@ -1,4 +0,0 @@
-def calculate_wallpaper(area, price_per_roll):
-    rolls_needed = area / 5  # Предположим, что один рулон покрывает 5 м²
-    total_cost = rolls_needed * price_per_roll
-    return rolls_needed, total_cost
